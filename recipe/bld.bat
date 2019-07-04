9d8deaaf2302a414bef7564d8e27ef628e0daa79
Julien Schueller <schueller@phimeca.com>
2019-07-04 18:15:53 +0200

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,20 +1,12 @@
-set CXXFLAGS=
-set CFLAGS=
+mkdir build && pushd build
 
-mkdir build
-pushd build
-
-cmake -G "%CMAKE_GENERATOR%" -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -D CMAKE_BUILD_TYPE=Release ..
+cmake -G"NMake Makefiles" -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -D CMAKE_BUILD_TYPE=Release ..
 if errorlevel 1 exit 1
 
-:: Build.
-cmake --build . --config Release
+:: Build/Install.
+cmake --build . --config Release --target install
 if errorlevel 1 exit 1
 
 :: Test.
 ctest -C Release
-if errorlevel 1 exit 1
-
-:: Install.
-cmake --build . --config Release --target install
 if errorlevel 1 exit 1
